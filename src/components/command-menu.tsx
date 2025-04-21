2323780d8e96fe43c5a67c94d494c5487398cc49
Baron Chong <baronchong23@gmail.com>
2025-04-21 23:55:44 +0800

    feat: update command menu to handle platform (pdf)detection and rendering

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -20,10 +20,13 @@ interface Props {
 
 export const CommandMenu = ({ links }: Props) => {
   const [open, setOpen] = React.useState(false);
-  let isMac =
-    typeof window !== "undefined"
-      ? window.navigator.userAgent.indexOf("Mac") > -1
-      : false;
+  const [isMac, setIsMac] = React.useState(false);
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+    setIsMac(window.navigator.userAgent.indexOf("Mac") > -1);
+  }, []);
 
   React.useEffect(() => {
     const down = (e: KeyboardEvent) => {
@@ -42,7 +45,7 @@ export const CommandMenu = ({ links }: Props) => {
       <p className="fixed bottom-0 left-0 right-0 hidden border-t border-t-muted bg-white p-1 text-center text-sm text-muted-foreground xl:block print:hidden">
         Press{" "}
         <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-          <span className="text-xs">{isMac ? "⌘" : "Ctrl"}</span>+J
+          <span className="text-xs">{mounted ? (isMac ? "⌘" : "Ctrl") : "Ctrl"}</span>+J
         </kbd>{" "}
         to open the command menu
       </p>
